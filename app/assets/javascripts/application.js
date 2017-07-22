91e9fab5f06b42f8252211b6626737afa5ec843d
Billy.Zheng <zw963@163.com>
2017-07-22 23:17:09 +0800

    Add opal-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+// Require the opal runtime and core library
+//= require opal
+
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-//= require 'opal-browser'
